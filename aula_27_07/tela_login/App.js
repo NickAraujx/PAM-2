fa7230c2d047d31c2e499c4dda2a6d8979c2b7a4
NickAraujx <nickmaster340@gmail.com>
2026-08-02 00:57:22 +0000

    01_08

--- aula_27_07/tela_login/App.js
+++ aula_27_07/tela_login/App.js
@@ -1,52 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Text, Image, ScrollView, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-
-  const [loading, setLoading] = useState(false);
-
   return (
-    <ScrollView style={styles.container}>
-
-      <Image
-          source={{
-            uri: 'https://br.pinterest.com/pin/766737905279854532/',
-          }}
-          style={{width: 200, height: 200}}
-        />
-
-      <Text>Digite seu email cadastrado:</Text>
-      <TextInput defaultValue="email@gmail.com" ></TextInput>
-
-      <Text>Digite sua senha cadastrada:</Text>
-      <TextInput defaultValue="senha123#" ></TextInput>
-
-      <Button
-        title={loading ? "Entrar" : "Entrando..."}
-        onPress={() => setLoading(true)}
-        disabled={loading}
-      />
-
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </ScrollView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flexGrow: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    flex: 1,
     backgroundColor: '#fff',
-    padding: 20,
-  },
-
-  input: {
-    width: '80%',
-    borderWidth: 1,
-    borderColor: '#999',
-    padding: 10,
-    marginBottom: 15,
-    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
