3efea04b4dbfbbdde3e5441016b86dd03da4b508
NickAraujx <nickmaster340@gmail.com>
2026-08-03 11:26:09 +0000

    03_08

--- aula_27_07/tela_login/App.js
+++ aula_27_07/tela_login/App.js
@@ -2,19 +2,50 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [loading, setLoading] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Digite seu email:</Text>
+    <ScrollView style={styles.container}>
+
+      <Image
+          source={{
+            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+          }}
+          style={{width: 200, height: 200}}
+        />
+
+      <Text>Digite seu email cadastrado:</Text>
+      <TextInput defaultValue="email@gmail.com" ></TextInput>
+
+      <Text>Digite sua senha cadastrada:</Text>
+      <TextInput defaultValue="senha123#" ></TextInput>
+
+      <Button
+        title={loading ? "Entrar" : "Entrando..."}
+        onPress={() => setLoading(true)}
+        disabled={loading}
+      />
+
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flexGrow: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    padding: 20,
+  },
+
+  input: {
+    width: '80%',
+    borderWidth: 1,
+    borderColor: '#999',
+    padding: 10,
+    marginBottom: 15,
+    borderRadius: 5,
   },
 });
